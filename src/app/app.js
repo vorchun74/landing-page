db019607850061e7d20ae81f71d57a3716c44602
vorchun74 <spa7452@gmail.com>
2021-03-22 00:04:06 +0300

    A lot of visual fixes, added JS listeners and url for links

--- src/app/app.js
+++ src/app/app.js
@@ -1,41 +1,83 @@
-const burgerMenu = document.querySelector('.burger');
-const header = document.querySelector('.header');
+function clickHandler(event) {
+  if (event.target.dataset.filter) {
+    sortHandler(event);
+  } else if (event.target.dataset.modal === 'modalProject') {
+    projectHandler(event);
+  } else if (event.target.dataset.close) {
+    closeHandler(event);
+  } else if (event.target.closest('.burger')) {
+    burgerHandler(event);
+  } else if (event.target.dataset.navigation) {
+    scrollHandler(event);
+  } else return;
+}
 
-burgerMenu.onclick = () => {
-  burgerMenu.classList.toggle('active');
-  header.classList.toggle('active');
+// Sort
+function sortHandler(event) {
+  event.preventDefault();
+  const projects = document.querySelectorAll('[data-category]');
+  let category = event.target.dataset.filter;
+
+  projects.forEach((project) => {
+    if (category === project.dataset.category || category === 'all') {
+      project.classList.remove('hide');
+    } else {
+      project.classList.add('hide');
+    }
+  });
 }
 
 // Modal window
-
-const modalBtn = document.querySelectorAll("[data-modal]");
-const modalWindows = document.querySelectorAll("[data-window]");
-const bodyScroll = document.querySelector("body");
-
-for (let i = 0; i < modalBtn.length; i++) {
-  modalBtn[i].addEventListener('click', 
-  function(event) {
-    event.preventDefault();
-    const modalType = modalBtn[i].dataset.modal;
-  
-    for (let j = 0; j < modalWindows.length; j++) {
-      if (modalType === modalWindows[j].dataset.window) {
-        const parentino = document.getElementById(modalType);
-        parentino.classList.add("show");
-        bodyScroll.classList.add("no-scroll");
-      } 
-    }
-  })
+function projectHandler(event) {
+  event.preventDefault();
+  document.getElementById('modalProject').classList.add('show');
+  document.querySelector('body').classList.add('no-scroll');
+}
+// Close button
+function closeHandler(event) {
+  event.preventDefault();
+  event.target.closest('.show').classList.remove('show');
+  document.querySelector('body').classList.remove('no-scroll');
 }
 
-const closeBtn = document.querySelectorAll("[data-close]");
-
-for (let i = 0; i < closeBtn.length; i++) {
-  closeBtn[i].addEventListener('click', function(event) {
-      event.preventDefault();
-
-    const currentWindow = closeBtn[i].closest(".show");
-    currentWindow.classList.remove("show");
-    bodyScroll.classList.remove("no-scroll");
-  })
+// Burger button
+function burgerHandler(event) {
+  event.preventDefault();
+  document.querySelector('.burger').classList.toggle('active');
+  document.querySelector('.header').classList.toggle('active');
 }
+
+// Scroll
+const navCoords = {
+  intro: 0,
+  about: 667,
+  skills: 1302,
+  works: 1852,
+  contact: 2406,
+};
+
+function scrollHandler(event) {
+  event.preventDefault();
+  let offsetPosition;
+  let targetOffset = navCoords[event.target.dataset.navigation];
+  if (pageYOffset === 0) {
+    offsetPosition = targetOffset;
+  } else if (pageYOffset > 0 && pageYOffset < targetOffset) {
+    offsetPosition = targetOffset - pageYOffset;
+  } else if (pageYOffset > targetOffset) {
+    offsetPosition = 0 - pageYOffset + targetOffset;
+  }
+  window.scrollBy({
+    top: offsetPosition,
+    behavior: 'smooth',
+  });
+}
+
+document.addEventListener('click', clickHandler);
+window.addEventListener('scroll', () => {
+  if (pageYOffset >= 2372) {
+    document.querySelector('.header').classList.add('hide');
+  } else {
+    document.querySelector('.header').classList.remove('hide');
+  }
+});
--- src/index.js
+++ src/index.js
@@ -2,5 +2,3 @@ import './styles/style.css';
 import './styles/style.scss';
 import './app/loadbar';
 import './app/app'
-
-console.log('Landos is about to begin!');
